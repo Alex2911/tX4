90cbb647e8da2a3e25f7f6ecc4a7d72fb1482236
tHeStRyNg <stryng@gmail.com>
2012-02-22 05:36:25 +0000

    Registration to work with several Subdomains

--- GameEngine/Mailer.php
+++ GameEngine/Mailer.php
@@ -1,57 +1,50 @@
 <?php
-#################################################################################
-##                                                                             ##
-##              -= YOU MUST NOT REMOVE OR CHANGE THIS NOTICE =-                ##
-##                                                                             ##
-## --------------------------------------------------------------------------- ##
-##                                                                             ##
-##  Project:       ZravianX                                                    ##
-##  Version:       2011.11.07                                                  ##
-##  Filename:      GameEngine/Mailer.php                                       ##
-##  Developed by:  Dixie                                                       ##
-##  Edited by:     ZZJHONS                                                     ##
-##  License:       Creative Commons BY-NC-SA 3.0                               ##
-##  Copyright:     ZravianX (c) 2011 - All rights reserved                     ##
-##  URLs:          http://zravianx.zzjhons.com                                 ##
-##  Source code:   http://www.github.com/ZZJHONS/ZravianX                      ##
-##                                                                             ##
-#################################################################################
-
 class Mailer {
-	
-	function sendActivate($email,$username,$pass,$act) {
-		
-		$subject = "Welcome to ".SERVER_NAME;
-		
-		$message = "Hello ".$username."
 
-Thank you for your registration.
+        function sendActivate($email,$username,$pass,$act) {
 
-The server will start the Monday 7, October, 2011 at 20:00 GMT+2
+                $subject = "Welcome to RavianX world ".SERVER_NAME;
+
+                $message = "Hello ".$username."
+
+Thank you for registering your RavianX account!
+
+You signed up for RavianX world ".SERVER_NAME". To activate your account please click the
+following link:
+
+".SERVER_NAME"".HOMEPAGE."activate.php?code=".$act."
+
 
 ----------------------------
+
+Your access data:
+
 Name: ".$username."
 Password: ".$pass."
-Activation code: ".$act."
-----------------------------
+Game world: ".SERVER_NAME"
 
-Click the following link in order to activate your account:
-".HOMEPAGE."activate.php?code=".$act."
+Activation code: ".$act."
+
+----------------------------
 
 Server homepage: ".HOMEPAGE."
 Server tutorial: ".HOMEPAGE."/#tutorial
-Server screenshots: ".HOMEPAGE."/#screenshots
 
-Greetings,
-ZravianX";
-				
-		$headers = "From: activate@".SERVER_NAME."\n";
-		//$headers .= 'MIME-Version: 1.0' . "\r\n";
-		//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		
-		mail($email, $subject, $message, $headers);
-	}
-	
+We have dedicated Staff working 24/7/365 in all our departments, so you will receive a reply in 20-30 minutes at any time of the day.
+
+Please do let us know if you have any questions.
+
+Regards,
+Support Team
+support@ravianx.com";
+
+                $headers = "From: Gate-Keeper@ravianx.com"\n";
+                //$headers .= 'MIME-Version: 1.0' . "\r\n";
+                //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+                mail($email, $subject, $message, $headers);
+        }
+
 };
 $mailer = new Mailer;
 ?>
